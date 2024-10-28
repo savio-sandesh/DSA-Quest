0f5387890e7af392870e12521772318573129eec
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2024-10-28 09:24:51 +0530

    [1539] [Time Beats: 100.00%] [Memory Beats: 27.10%] - LeetPush

---
+++ 1539-Kth-Missing-Positive-Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        for(int i:arr){
+            if(i<=k){
+                k++;
+            }
+            else{
+                break;
+            }
+        }
+        return k;
+    }
+}
