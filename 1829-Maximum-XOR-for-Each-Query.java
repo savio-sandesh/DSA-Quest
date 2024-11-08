8f86e12173f2b15522500f91579d6b5715acbf82
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2024-11-09 02:26:29 +0530

    [1829] [Time Beats: 79.49%] [Memory Beats: 6.19%] - LeetPush

---
+++ 1829-Maximum-XOR-for-Each-Query.java
@@ -0,0 +1,5 @@
+class Solution {
+    public int[] getMaximumXor(int[] nums, int maximumBit) {
+        int e = (int) Math.pow(2, maximumBit); int[] prexor = new int[nums.length]; prexor[0] = nums[0]; for (int i = 1; i < nums.length; i++) { prexor[i] = prexor[i - 1] ^ nums[i]; } int[] res = new int[nums.length]; for (int i = prexor.length - 1, j = 0; i >= 0; i--, j++) { res[j] = (e - 1) ^ prexor[i]; } return res;
+    }
+}
