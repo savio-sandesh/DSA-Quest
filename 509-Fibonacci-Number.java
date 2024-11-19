c224659acaf015a6e622f4185144167ecc8bdfd8
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2024-11-20 03:16:40 +0530

    [509] [Time Beats: 100.00%] [Memory Beats: 29.56%] - LeetPush

--- 509-Fibonacci-Number.java
+++ 509-Fibonacci-Number.java
@@ -1,11 +1,13 @@
 class Solution {
     public int fib(int n) {
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        return fib(n-1)+fib(n-2);
+        // if(n==0) return 0;
+        // if(n==1) return 1;
+        // return fib(n-1)+fib(n-2);
+
+        //Binet's Formula:
+    
+        double sqrt5 = Math.sqrt(5);
+        double fibN = Math.pow((1 + sqrt5) / 2, n) - Math.pow((1 - sqrt5) / 2, n);
+        return (int) Math.round(fibN / sqrt5);
     }
 }
