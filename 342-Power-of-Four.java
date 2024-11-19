26e87d7d4e5cc04121e28a2875218ed7daf442aa
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2024-11-20 03:12:11 +0530

    [342] [Time Beats: 100.00%] [Memory Beats: 64.67%] - LeetPush

--- 342-Power-of-Four.java
+++ 342-Power-of-Four.java
@@ -1,13 +1,17 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        
-        if(n<=0){
-            return false;
-        }
-        while(n%4==0){
-            n/=4;
-        }
-        return n==1;
+        // if(n<=0){
+        //     return false;
+        // }
+        // while(n%4==0){
+        //     n/=4;
+        // }
+        // return n==1;
+
+        // recursive approach
+        if(n<=0) return false;
+        if(n==1) return true;
+        return (n%4==0) && isPowerOfFour(n/4);
 
     }
 }
