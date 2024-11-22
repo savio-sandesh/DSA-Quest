b298fa02165580e67622f7d20a919ad4116c9c51
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2024-11-22 10:56:16 +0530

    [62] [Time Beats: 100.00%] [Memory Beats: 65.37%] - LeetPush

--- 62-Unique-Paths.java
+++ 62-Unique-Paths.java
@@ -1,16 +1,30 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int[][] arr=new int [m][n];
-        for(int i =0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 || j==0){
-                    arr[i][j]=1;
-                }
-                else{
-                    arr[i][j]=arr[i-1][j]+arr[i][j-1];
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        
+        if (obstacleGrid[0][0] == 1) return 0;
+        
+        obstacleGrid[0][0] = 1;
+        
+        for (int i = 1; i < m; i++) {
+            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) ? 1 : 0;
+        }
+        
+        for (int j = 1; j < n; j++) {
+            obstacleGrid[0][j] = (obstacleGrid[0][j] == 0 && obstacleGrid[0][j - 1] == 1) ? 1 : 0;
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+                } else {
+                    obstacleGrid[i][j] = 0;
                 }
             }
         }
-        return arr[m-1][n-1];
+        
+        return obstacleGrid[m - 1][n - 1];
     }
 }
