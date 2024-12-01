9542d25da1c8fb5c0102ab91798366a81c8195a7
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2024-12-01 11:35:10 +0530

    [1346] [Time Beats: 93.52%] [Memory Beats: 94.02%] - LeetPush

--- 1346-Check-If-N-and-Its-Double-Exist.java
+++ 1346-Check-If-N-and-Its-Double-Exist.java
@@ -2,9 +2,6 @@ class Solution {
     public boolean checkIfExist(int[] arr) {
         int n =arr.length;
         for(int i =0;i<n;i++){
-            // if(arr[i]==0){
-            //     return false;
-            // }
             for(int j =0;j<n;j++){
               if(i!=j && arr[i]==2*arr[j]){
                 return true;
