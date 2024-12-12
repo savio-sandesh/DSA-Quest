cf4da77b1f83ec12e8167cef46217f3f50e3b8de
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2024-12-12 19:20:04 +0530

    [15] [Time Beats: 43.25%] [Memory Beats: 78.62%] - LeetPush

---
+++ 15-3Sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length < 3) return res;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1, k = nums.length - 1, target = -nums[i];
+            while (j < k) {
+                int sum = nums[j] + nums[k];
+                if (sum == target) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while (j < k && nums[j] == nums[++j]);
+                    while (j < k && nums[k] == nums[--k]);
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return res;
+    }
+}
