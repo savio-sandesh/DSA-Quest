add8a138cebd04aedeb973d76e07315a52f2a245
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2025-01-08 19:14:23 +0530

    [739] [Time Beats: 47.77%] [Memory Beats: 24.77%] - LeetPush

---
+++ 739-Daily-Temperatures.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n= temperatures.length;
+        Stack<Integer> st = new Stack<>();
+        int[] ans=new int[n];
+        for(int i =0;i<n;i++){
+            // int count=0;
+            // for(int j =i+1;j<n;j++){
+            //     if(temperatures[j]>temperatures[i]){
+            //         ans[i]=count;
+            //         break;
+            //     }
+            // } 
+            while(!st.isEmpty() && temperatures[i]>temperatures[st.peek()]){
+                int id = st.pop();
+                ans[id]=i-id;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+}
