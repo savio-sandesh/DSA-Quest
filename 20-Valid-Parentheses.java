d6b29da756c6b897ab27faebc9e8d58c3b0ee07d
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2025-02-04 10:01:36 +0530

    [20] [Time Beats: 42.01%] [Memory Beats: 91.49%] - LeetPush

--- 20-Valid-Parentheses.java
+++ 20-Valid-Parentheses.java
@@ -1,19 +1,37 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) return false;
-                char top = stack.pop();
-                if (c == ')' && top != '(') return false;
-                if (c == '}' && top != '{') return false;
-                if (c == ']' && top != '[') return false;
+        Stack <Character> stack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(stack.isEmpty() || ch=='(' || ch=='[' || ch=='{'){
+                stack.push(ch);
+                continue;
+            }
+            if(ch==')'){
+                if(stack.peek()=='('){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(ch==']'){
+                if(stack.peek()=='['){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(ch=='}'){
+                if(stack.peek()=='{'){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
             }
         }
-        
         return stack.isEmpty();
     }
 }
