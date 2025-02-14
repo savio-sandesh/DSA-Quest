615b4b6297e089895ded06ee46358d1f398fb9d2
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2025-02-14 21:22:54 +0530

    [2349] [Time Beats: 68.18%] [Memory Beats: 37.64%] - LeetPush

---
+++ 2349-Design-a-Number-Container-System.java
@@ -0,0 +1,33 @@
+class NumberContainers {
+    // Space : O(n)
+    private Map<Integer, Integer> idxToNum;
+    private Map<Integer, PriorityQueue<Integer>> numToIdx;
+
+    public NumberContainers() {
+        idxToNum = new HashMap<>();
+        numToIdx = new HashMap<>();
+    }
+    
+    public void change(int index, int number) {
+        idxToNum.put(index, number); // O(1)
+        numToIdx.putIfAbsent(number, new PriorityQueue<>());
+        numToIdx.get(number).offer(index); // O(log(n))
+    }
+    
+    public int find(int number) {
+        if (!numToIdx.containsKey(number)) {
+            return -1;
+        }
+
+        PriorityQueue<Integer> pq = numToIdx.get(number);
+
+        while (!pq.isEmpty()) { // O(k * logn)
+            int idx = pq.peek(); // O(1)
+            if (idxToNum.get(idx) == number) {
+                return idx;
+            }
+            pq.poll(); // O(log(n))
+        }
+        return -1;
+    }
+}
