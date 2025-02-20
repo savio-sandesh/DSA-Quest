a696d6c7437b08102f000e52a7a0c25c7ef707d8
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2025-02-21 01:12:26 +0530

    [704] [Time Beats: 100.00%] [Memory Beats: 98.89%] - LeetPush

--- 704-Binary-Search.java
+++ 704-Binary-Search.java
@@ -1,19 +1,37 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low=0;
-        int high = nums.length-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
+        int s=0;
+        int e = nums.length-1;
+        return recur(nums,s,target,e);
+    }
+        
+    public int recur(int[] nums,int s,int target,int e){
+        if(s>e){
+            return -1;
+            }
+            int mid=s+(e-s)/2;
             if(nums[mid]==target){
                 return mid;
+            }else if(target>nums[mid]){
+                return recur(nums,mid+1,target,e);
+            }else{
+                return recur(nums,s,target,mid-1);
             }
-            else if(nums[mid]<target){
-                low =mid+1;
-             }
-            else{
-                high=mid-1;
-            }
-        }
-        return -1;
     }
 }
+
+// iterative approach
+
+// while(low<=high){
+        //     int mid = low + (high-low)/2;
+        //     if(nums[mid]==target){
+        //         return mid;
+        //     }
+        //     else if(nums[mid]<target){
+        //         low =mid+1;
+        //      }
+        //     else{
+        //         high=mid-1;
+        //     }
+        // }
+        // return -1;
