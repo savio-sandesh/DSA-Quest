21dbbdc6f025e1d3cf3cf6223dac9533be93dd7b
Sandesh Yadav <127104160+savio-sandesh@users.noreply.github.com>
2025-03-06 23:51:50 +0530

    [2965] [Time Beats: 24.14%] [Memory Beats: 80.11%] - LeetPush

---
+++ Find-Missing-and-Repeated-Values.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length;
+        int N = n * n; // total elements
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int a = -1;
+        int b = -1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map.put(grid[i][j], map.getOrDefault(grid[i][j], 0) + 1);
+            }
+        }
+
+        //[1...N]
+        for (int num = 1; num <= N; num++) {
+            if (!map.containsKey(num)) {
+                b = num;
+            } else if (map.get(num) == 2) {
+                a = num;
+            }
+
+            if (a != -1 && b != -1) {
+                break;
+            }
+        }
+
+        return new int[]{a, b};
+    }
+}
